e95964ed948ea6e832de5c6d5c4915daef2b1de6
rowshansa <rowshan_sathi@yahoo.com>
2016-07-27 14:04:08 +0200

    dashboard_view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  #http_basic_authenticate_with :name => "frodo", :password => "thering"
+
 
   # GET /sessions/new
   def new
@@ -10,9 +12,20 @@ class SessionsController < ApplicationController
   # POST /sessions.json
   def create
     #user auth
-    user= ApiM8::Resources::Accounts::User.new(:login=>params[:login],:password=>params[:password])
-    session[:current_user_id]=user.id
-    redirect_to home_index_url
+    user=  ApiM8::Resources::Accounts::User.new(:login=>params[:login],:password=>params[:password])
+   # user= ApiM8::Resources::Accounts::User.find_by_login(:login => params[:login])
+
+   # if user.is_a?ApiM8::Resources::Accounts::User
+    if user #&& user.authenticate
+
+      session[:current_user_id]=user.id
+      redirect_to role_super_admin_dashboards_url, :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+
+      render "new"
+    end
+
   end
 
   # DELETE /sessions/1
