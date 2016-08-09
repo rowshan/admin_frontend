fae1f5f0e13677a480e6b00418a96ccb6973523c
rowshansa <rowshan_sathi@yahoo.com>
2016-08-09 15:31:24 +0200

    reset password for user login

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,18 @@
 class PasswordResetsController < ApplicationController
+  def index
+  end
+
+  def show
+
+  end
+
   def new
   end
 
   def create
+    user = ApiM8::Resources::Accounts::User.new(:login=>params[:login])
+    user.password_reset if user
+    redirect_to root_url, :notice => "Email sent with password reset instructions."
   end
 
   def update
