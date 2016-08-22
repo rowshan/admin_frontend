6154a1f7a0fe99f3c23866957b657309831e3876
rowshansa <rowshan_sathi@yahoo.com>
2016-08-22 10:06:33 +0200

    fixes for user signup for ticket- JFADM-2

--- app/controllers/role/super_admin_dashboards_controller.rb
+++ app/controllers/role/super_admin_dashboards_controller.rb
@@ -2,27 +2,32 @@ class Role::SuperAdminDashboardsController < ApplicationController
   def index
   end
 
-  # def new
-  #   #create signup_form for new user
-  #   @role_options= ApiM8::Resources::Accounts::Role.all.map{|r| [r.name]}
-  #
-  # end
-
-  # def create
-  #   #user auth
-  #   #@user= ApiM8::Resources::Accounts::User.new(params[:login],params[:password],params[:confirmation])
-  #   @user= ApiM8::Resources::Accounts::User.new.create(params[:user])
-  #
-  #
-  #
-  # end
-
-   # def show
-   #
-   # end
-  #private
-  # def user_params
-  #   re
-  # end
+  def new
+    #create signup_form for new user
+    @user= ApiM8::Resources::Accounts::User.new
 
   end
+
+  def create
+    #user auth
+    #@user= ApiM8::Resources::Accounts::User.new(params[:login],params[:password],params[:confirmation])
+    @user= ApiM8::Resources::Accounts::User.new.create(params[:user])
+    debugger
+    if @user.save
+
+      redirect_to role_super_admin_dashboards_path @user.id
+    else
+      render "new", :notice => 'User exists.Select another user name '
+    end
+
+
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:login, :password, :password_confirmation, :role_id, :tenant_id)
+  end
+
+
+end
