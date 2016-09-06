b3e64f5f4b0db5f024fb7bc70074042e44654916
rowshansa <rowshan_sathi@yahoo.com>
2016-09-06 09:59:30 +0200

    Edit, show, delete added to the customer table  for ticket- JFADM-5

--- app/controllers/role/customers_controller.rb
+++ app/controllers/role/customers_controller.rb
@@ -1,13 +1,35 @@
 class Role::CustomersController < ApplicationController
   def index
-   # @profiles= ApiM8::Resources::Accounts::Profile.new nil,nil
+    # @profiles= ApiM8::Resources::Accounts::Profile.new nil,nil
     #list=@profiles.list.map{|e|[e.id,e.user_id,e.first_name,e.last_name,e.date_of_birth,e.country_of_residence]}
     #hash=eval(list)
+    # profiles=Hash.new
+    #
+    # profiles[:first_name]="Anne"
+    # profiles[:last_name]='Marry'
+    # profiles[:date_of_birth]= '01.01.1970'
+    # profiles[:country_of_residence]='Germany'
 
+
+    #generate a hash here
+    @profiles={
+        :id=> 1,
+        :first_name=>"Anne",
+        :last_name=>"Marry",
+        :date_of_birth=>'01.01.1970',
+        :country_of_residence=>'Germany'
+    }
+
+
+
+    #puts profiles
+    #list=profiles.map{|e | [e.first_name,e.las]}
+    #puts list
     #render json: @profiles
   end
 
   def show
+    @profile=@profiles.find(@profiles[:id])
 
   end
 
