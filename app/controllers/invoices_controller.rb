95d977754a6db0ad87607760db1d8f43479dd3cf
rowshansa <rowshan_sathi@yahoo.com>
2016-09-07 14:54:51 +0200

    fixes for  ticket- JFADM-5

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,28 +1,41 @@
 class InvoicesController < ApplicationController
   def index
-   # @invoices= ApiM8::Resources::Books::Invoice.new nil,nil,({:id=>params[:id],
-   #                                                          :invoice_date=>params[:invoice_date],
-   #                                                          :invoice_no=>params[:invoice_no],
-   #                                                          :user_id=>params[:user_id],
-   #                                                          :vat=>params[:vat],
-   #                                                          :total_amount=>params[:total_amount],
-   #                                                          :tenant_id=>current_context.tenant_id
-   # })
+    # @invoices= ApiM8::Resources::Books::Invoice.new nil,nil,({:id=>params[:id],
+    #                                                          :invoice_date=>params[:invoice_date],
+    #                                                          :invoice_no=>params[:invoice_no],
+    #                                                          :user_id=>params[:user_id],
+    #                                                          :vat=>params[:vat],
+    #                                                          :total_amount=>params[:total_amount],
+    #                                                          :tenant_id=>current_context.tenant_id
+    # })
 
-    @invoices={:id => "def-3434dsds",
+    @invoices={:id => "1",
                :invoice_date => '12-23-1984',
-               :invoice_no => "dweew", :user_id => session[:current_user_id],
+               :invoice_no => "dweew",
+               :user_id => session[:current_user_id],
                :vat => 1.345,
                :total_amount => 30.456,
-               :tenant_id => current_context.tenant_id
+               :tenant_id => 'ten-1' #current_context.tenant_id
     }
-   puts @invoices[:id]
+    puts @invoices[:id]
 
-   # render json: @invoices
+    # render json: @invoices
   end
 
   def show
-     #render json: @invoices
+    @invoices={:id => "1",
+               :invoice_date => '12-23-1984',
+               :invoice_no => "dweew",
+               :user_id => session[:current_user_id],
+               :vat => 1.345,
+               :total_amount => 30.456,
+               :tenant_id => 'ten-1' #current_context.tenant_id
+    }
+
+    @invoice=@invoices.each do|k,v|
+      #{k}:#{v}
+    end
+   # render json: @invoice
 
   end
 
