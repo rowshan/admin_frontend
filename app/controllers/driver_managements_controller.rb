0263e5601a7a6bda656430413c62f6693654c048
rowshansa <rowshan_sathi@yahoo.com>
2016-09-07 16:05:56 +0200

    fixes for ticket- JFADM-13, JFADM-14

--- app/controllers/driver_managements_controller.rb
+++ app/controllers/driver_managements_controller.rb
@@ -1,8 +1,29 @@
 class DriverManagementsController < ApplicationController
   def index
+    @drivers={:id=>1,
+              :first_name=>'JJ',
+              :last_name=>'Dove',
+              :phone_no=>'123232323',
+              :plant_id=>'plant-1',
+              :enabled=>'yes'
+    }
+    #render json: @drivers
+
   end
 
   def show
+    @drivers={:id=>1,
+              :first_name=>'JJ',
+              :last_name=>'Dove',
+              :phone_no=>'123232323',
+              :plant_id=>'plant-1',
+              :enabled=>'yes'
+    }
+
+    @driver=@drivers.each do|k,v|
+      #{k}:#{v}
+
+    end
   end
 
   def edit
