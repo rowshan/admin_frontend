252015ea21aa34340c202a132c64a67326026b33
rowshansa <rowshan_sathi@yahoo.com>
2016-09-07 16:51:52 +0200

    fixes for ticket- JFADM-15, JFADM-16

--- app/controllers/country_managements_controller.rb
+++ app/controllers/country_managements_controller.rb
@@ -1,8 +1,21 @@
 class CountryManagementsController < ApplicationController
   def index
+    @countries={
+    :id=>1,
+    :name=>'Germany',
+    :code=>'DE'
+    }
   end
 
   def show
+    @countries={
+        :id=>1,
+        :name=>'Germany',
+        :code=>'DE'
+    }
+    @country=@countries.each do |k,v|
+      #{k}#{v}
+    end
   end
 
   def edit
