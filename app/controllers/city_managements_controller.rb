c9f14ce828134f59948d99e9a0039730227fe43d
rowshansa <rowshan_sathi@yahoo.com>
2016-09-08 09:41:36 +0200

    fixes for ticket- JFADM-19, JFADM-20

--- app/controllers/city_managements_controller.rb
+++ app/controllers/city_managements_controller.rb
@@ -1,8 +1,20 @@
 class CityManagementsController < ApplicationController
   def index
+    @cities={:id => 1,
+             :name => 'Berlin',
+             :slug => 'berlin',
+             :province_id => 'prov-1'}
   end
 
   def show
+    @cities={:id => 1,
+             :name => 'Berlin',
+             :slug => 'berlin',
+             :province_id => 'prov-1'}
+
+    @city=@cities.each do |k, v|
+      #{k}:#{v}
+    end
   end
 
   def edit
