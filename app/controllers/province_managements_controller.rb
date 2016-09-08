9469f3d4c9e5cbe196f59c2bd05ff78fa6e0a8ab
rowshansa <rowshan_sathi@yahoo.com>
2016-09-08 09:22:24 +0200

    fixes for ticket- JFADM-17, JFADM-18

--- app/controllers/province_managements_controller.rb
+++ app/controllers/province_managements_controller.rb
@@ -1,8 +1,19 @@
 class ProvinceManagementsController < ApplicationController
   def index
+    @provinces={:id=>1,
+    :name=>'Berlin',
+    :slug=>'berlin'}
   end
 
   def show
+    @provinces={:id=>1,
+                          :name=>'Berlin',
+                          :slug=>'berlin'}
+
+    @province=@provinces.each do |k,v|
+      #{k}:#{v}
+    end
+
   end
 
   def edit
