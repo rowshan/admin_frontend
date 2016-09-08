87ad0d06651f28060cf2a09a37ce8927d0f49341
rowshansa <rowshan_sathi@yahoo.com>
2016-09-08 10:52:38 +0200

    fixes for ticket JFADM-25, JFADM-26

--- app/controllers/shift_managements_controller.rb
+++ app/controllers/shift_managements_controller.rb
@@ -1,8 +1,31 @@
 class ShiftManagementsController < ApplicationController
   def index
+    @shifts={
+        :id=>1,
+        :name=>'morning',
+        :enabled=>'no',
+        :start_time=>'2:00 pm',
+        :end_time=>'00:00 am',
+        :plant_id=>'plant-1'
+
+
+
+    }
   end
 
   def show
+    @shifts={
+        :id=>1,
+        :name=>'morning',
+        :enabled=>'no',
+        :start_time=>'2:00 pm',
+        :end_time=>'00:00 am',
+        :plant_id=>'plant-1'
+
+    }
+    @shift=@shifts.each do |k,v|
+      #{k} #{v}
+    end
   end
 
   def new
