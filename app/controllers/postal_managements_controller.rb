725edbbeb5ee2496e510888696962a5a0a6251cc
rowshansa <rowshan_sathi@yahoo.com>
2016-09-08 10:07:59 +0200

    fixes for ticket- JFADM-21, JFADM-22

--- app/controllers/postal_managements_controller.rb
+++ app/controllers/postal_managements_controller.rb
@@ -1,8 +1,20 @@
 class PostalManagementsController < ApplicationController
   def index
+    @postals={:id => 1,
+              :code => '10965',
+              :city_id => 'city-1'
+    }
   end
 
   def show
+    @postals={:id => 1,
+              :code => '10965',
+              :city_id => 'city-1'
+    }
+
+    @postal=@postals.each do |k, v|
+      #{k}:#{v}
+    end
   end
 
   def edit
