92f30b46ac3d34ec92d2babdbb2dddb1abc275bb
rowshansa <rowshan_sathi@yahoo.com>
2016-09-08 10:29:19 +0200

    fixes for ticket- JFADM-23, JFADM-24

--- app/controllers/time_window_managements_controller.rb
+++ app/controllers/time_window_managements_controller.rb
@@ -1,8 +1,27 @@
 class TimeWindowManagementsController < ApplicationController
   def index
+    @time_windows={
+        :id=>1,
+        :label=>'sasasa',
+        :tenant_id=>'ten-1',
+        :start_time=>'2:00 pm',
+        :end_time=>'00:00 am'
+
+    }
   end
 
   def show
+    @time_windows={
+        :id=>1,
+        :label=>'sasasa',
+        :tenant_id=>'ten-1',
+        :start_time=>'2:00 pm',
+        :end_time=>'00:00 am'
+
+    }
+    @time_window=@time_windows.each do |k,v|
+      #{k} #{v}
+    end
   end
 
   def edit
