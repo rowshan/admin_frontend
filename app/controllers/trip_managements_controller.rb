04e6c5753a75812c2cceed5d610f33af4f4e1377
rowshansa <rowshan_sathi@yahoo.com>
2016-09-08 11:11:03 +0200

    Trip update for tickets JFADM-27, JFADM-28

--- app/controllers/trip_managements_controller.rb
+++ app/controllers/trip_managements_controller.rb
@@ -1,8 +1,26 @@
 class TripManagementsController < ApplicationController
+
   def index
+    @trips={
+        :id => 1,
+        :trip_kind_id => 'wash-1',
+        :order_id => 'order-1',
+        :date => '12/09/2016 2:00 pm',
+        :time_window_id => 't-1'
+    }
   end
 
   def show
+    @trips={
+        :id => 1,
+        :trip_kind_id => 'wash-1',
+        :order_id => 'order-1',
+        :date => '12/09/2016 2:00 pm',
+        :time_window_id => 't-1'
+    }
+    @trip=@trips.each do |k, v|
+      #{k} #{v}
+    end
   end
 
   def new
