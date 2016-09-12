53975934d7fa0fed99c6c2c35a09d004160ef2de
rowshansa <rowshan_sathi@yahoo.com>
2016-09-12 11:09:49 +0200

    Show invoice item for ticket- JFADM-9

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,42 +1,15 @@
 class InvoicesController < ApplicationController
   def index
     @invoices= ApiM8::Client::Books::Invoices.instance.index
-    #  # (:id=>params[:id],
-    #                                                          :invoice_date=>params[:invoice_date],
-    #                                                          :invoice_no=>params[:invoice_no],
-    #                                                          :user_id=>params[:user_id],
-    #                                                          :vat=>params[:vat],
-    #                                                          :total_amount=>params[:total_amount],
-    #                                                          :tenant_id=>current_context.tenant_id
-    # )
 
-    # @invoices={:id => "1",
-    #            :invoice_date => '12-23-1984',
-    #            :invoice_no => "dweew",
-    #            :user_id => session[:current_user_id],
-    #            :vat => 1.345,
-    #            :total_amount => 30.456,
-    #            :tenant_id => 'ten-1' #current_context.tenant_id
-    # }
-    # puts @invoices[:id]
 
     # render json: @invoices
   end
 
   def show
-    @invoices={:id => "1",
-               :invoice_date => '12-23-1984',
-               :invoice_no => "dweew",
-               :user_id => session[:current_user_id],
-               :vat => 1.345,
-               :total_amount => 30.456,
-               :tenant_id => 'ten-1' #current_context.tenant_id
-    }
+    @invoice = ApiM8::Client::Books::Invoices.instance.show(params[:id])
 
-    @invoice=@invoices.each do|k,v|
-      #{k}:#{v}
-    end
-   # render json: @invoice
+    # render json: @invoice
 
   end
 
